40d682cfa2fea8ca7ad39ff38598c04b50476190
Hamza Neffati <hamza.neffati@1spatial.com>
2022-07-22 09:17:13 +0100

    integrate sass with simple exemple

--- src/pages/public/Home/Home.tsx
+++ src/pages/public/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { GET_COUNTRIES } from "../../../graphql/queries";
 import { useGQLQuery } from "../../../hooks/useGQLQuery";
+import "./Home.scss";
 
 export const Home = () => {
   const { status, data } = useGQLQuery("countries", GET_COUNTRIES);
@@ -12,10 +13,10 @@ export const Home = () => {
   }
 
   return (
-    <>
+    <div className="country">
       {data.countries.map((res: any) => (
         <p key={res.name}>{res.name}</p>
       ))}
-    </>
+    </div>
   );
 };
